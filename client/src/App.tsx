ea3a5c88c2559dfbcea60b0696250812258f3386
Lucky <luckygowda1718@gmail.com>
2024-10-06 15:48:17 +0530

    sd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,10 @@
 import BannerCategories from "./ui/BannerCategories"
-import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import HomeBanner from "./ui/HomeBanner";
 import Highlights from "./ui/Highlights";
 import Categories from "./ui/Categories";
 import ProductList from "./ui/ProductList";
 import DiscountedBanner from "./ui/DiscountedBanner";
-import Blog from "./ui/Blog";
-
 function App() {
 
   return (
